f8b6c064d6bd41b7845d866d5e55db93f195a57c
vamshi krishna <vamshi123nimmakuri123@gmail.com>
2018-02-01 15:11:08 +0530

    added twi feed , but a trouble

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import meetLogo from '../src/assets/meet.png';
 import gitLogo from '../src/assets/git.png';
 
 class App extends Component {
+  openLink(url){
+    window.open(url);
+  }
   render() {
     return (
       <div className=" img-fluid">
@@ -26,24 +29,27 @@ class App extends Component {
                   </h3>
                </ul>
                <hr></hr>
-               <ButtonToolbar className="btn-con" >
-                 <Button className="btn" bsSize="large"><img className=" img img-fluid" src={fbLogo} />facebook</Button>
-                 <Button className="btn" bsSize="large"><img className=" img img-fluid" src={twiLogo}/>twitter</Button>
-                 <Button className="btn" bsSize="large"><img className=" img img-fluid" src={meetLogo}/>meet-up</Button>
-                 <Button className="btn" bsSize="large"><img className=" img img-fluid" src={gitLogo}/>git-hub</Button>
+               <div className="btn-con" >
+                 <Button className="btn" onClick={()=>this.openLink("https://www.facebook.com/webdevhyd")}  bsSize="large"><img className=" img img-fluid" src={fbLogo} />facebook</Button>
+                 <Button className="btn" onClick={()=>this.openLink("https://twitter.com/webdevhyd")}  bsSize="large"><img className=" img img-fluid" src={twiLogo}/>twitter</Button>
+                 <Button className="btn" onClick={()=>this.openLink("https://www.meetup.com/WebDevHyd")}  bsSize="large"><img className=" img img-fluid" src={meetLogo}/>meet-up</Button>
+                 <Button className="btn" onClick={()=>this.openLink("https://github.com/webdevhyd")}  bsSize="large"><img className=" img img-fluid" src={gitLogo}/>git-hub</Button>
               
-               </ButtonToolbar>
+               </div>
                <hr/>
                <div className="feed-con" >
                  <div className="fb-feed" >
-                 <div class="fb-page" data-href="https://www.facebook.com/webdevhyd" data-tabs="timeline" data-width="600" data-height="450" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/webdevhyd" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/webdevhyd">Web Developers Hyderabad</a></blockquote></div>
+                 <div class="fb-page" data-href="https://www.facebook.com/webdevhyd" data-tabs="timeline" data-width="1100" data-height="200" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/webdevhyd" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/webdevhyd">Web Developers Hyderabad</a></blockquote></div>
                  </div>
-                 <div className="twi-feed" ></div>
+                 <div className="twi-feed" >
+                 <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/vamshik10139492?ref_src=twsrc%5Etfw">Tweets by vamshik10139492</a>
+                  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>   
                 
                  
                </div>
              
           </div>
+      </div>
       </div>
     );
   }
